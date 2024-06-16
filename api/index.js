42596426bfbb22779de730a8047e086653175bc5
aryan <aryansadeghyan54@gmail.com>
2024-06-16 16:35:09 +0330

    conected to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = 3000;
 const app = express();
 

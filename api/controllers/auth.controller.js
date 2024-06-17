7a4506bc9cd8b1917f5b2fd1f18e846ccb81f1b3
aryan <aryansadeghyan54@gmail.com>
2024-06-17 19:59:45 +0330

    signin route added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import prisma from "../../prisma/prismaClient.js";
-import bcrypt from "bcrypt";
+import bcrypt, { hashSync } from "bcrypt";
 import { errorHandeler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -19,6 +20,37 @@ export const signup = async (req, res, next) => {
     res.send({
       success: true,
       message: "user created successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { password, email } = req.body;
+
+  try {
+    const validUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!validUser) {
+      return next(errorHandeler(404, "user not found"));
+    }
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandeler(401, "wrong username or password"));
+    }
+
+    const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET);
+    res.cookie("access_token", token, { httpOnly: true });
+    delete validUser.password;
+    res.send({
+      success: true,
+      user: validUser,
+      token,
     });
   } catch (error) {
     next(error);

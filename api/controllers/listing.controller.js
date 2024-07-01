73cce39bfc27c9e817ddb2744c5b4c0a8f2c5605
aryan <aryansadeghyan54@gmail.com>
2024-07-01 14:42:38 +0330

    search api route added

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -117,3 +117,63 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getSearchedListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = undefined;
+    } else {
+      offer = offer === "true";
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = undefined;
+    } else {
+      furnished = furnished === "true";
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = undefined;
+    } else {
+      parking = parking === "true";
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = undefined;
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await prisma.listing.findMany({
+      where: {
+        name: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+        offer: offer === undefined ? undefined : offer,
+        furnished: furnished === undefined ? undefined : furnished,
+        parking: parking === undefined ? undefined : parking,
+        type: type === undefined ? undefined : type,
+      },
+      orderBy: {
+        [sort]: order,
+      },
+      take: limit,
+      skip: startIndex,
+    });
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   getListing,
   getListings,
+  getSearchedListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -15,5 +16,6 @@ router.get("/getuserlistings/:id", getListings);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:listingId", verifyToken, updateListing);
 router.get("/getlisting/:listingId", getListing);
+router.get("/getsearchedlistings", getSearchedListings);
 
 export default router;
